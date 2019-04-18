994e20cf4d21540d65fdd314d9e9b287a47687ee
Sam Ticknor <ticknors@carnegiemuseums.org>
2019-04-18 16:48:46 -0400

    rough draft of scroll

--- templates/Scroll.js
+++ templates/Scroll.js
@@ -1,51 +1,146 @@
-function loadScroll(page, pageTitle) {
-
-  updateMenu(pageTitle);
-  $("#page").empty();
-
-  $("#page").html("Scroll for "+pageTitle);
-}
-
 class Scroll extends Page {
+  subOptions;
+
+  constructor(data) {
+    super(data);
+
+    this.subOptions = Object.assign({}, this.data.subOptions);
+    this.subOptions[this.data.hash] = { "hash" : this.data.hash, rows: this.data.rows, title: this.data.title, index: 0, isThumbnailView: false };
+    for (var subOption in this.subOptions) {
+      this.subOptions[subOption].index = 0;
+    }
+  }
+
+  renderIntro(rowData) {
+    var intro = $("<div>");
+  	intro.addClass("scrollintro");
+
+  	if (rowData.imageurl) {
+  		var image = $("<img>");
+  		image.addClass("scrollIntroImage");
+  		image.attr("src", rowData.imageurl);
+  		intro.append(image);
+  	}
+
+  	if (rowData.caption) {
+  		var caption = $("<div>");
+  		caption.addClass("scrollIntroCaption");
+  		caption.html(rowData.caption);
+  		intro.append(caption);
+  	}
+
+  	if (rowData.heading || rowData.subheading) {
+  			var heading = $("<div>");
+  			console.log(makeDetailsLine(["<b>"+rowData.heading+"</b>", rowData.subheading]))
+  			heading.html(makeDetailsLine(["<b>"+rowData.heading+"</b>", rowData.subheading]));
+  			heading.addClass("scrollIntroHeading")
+  	    intro.append(heading);
+  	}
+
+  	if (rowData.text) {
+  		var text = $("<div>");
+  		text.addClass("scrollIntroText");
+  		text.html(rowData.text);
+  		intro.append(text);
+  	}
+
+  	$("#page").append(intro);
+  }
+
+
+  renderBlock(rowData) {
+  	var block = $("<div>");
+  	block.addClass("scrollBlock");
+
+  	if (rowData.heading || rowData.subheading) {
+  			var heading = $("<div>");
+  			console.log(makeDetailsLine(["<b>"+rowData.heading+"</b>", rowData.subheading]))
+  			heading.html(makeDetailsLine(["<b>"+rowData.heading+"</b>", rowData.subheading]));
+  			heading.addClass("scrollBlockHeading")
+  	    block.append(heading);
+  	}
+
+  	if (rowData.text) {
+  		var text = $("<div>");
+  		text.addClass("scrollBlockText");
+  		text.html(rowData.text);
+  		block.append(text);
+  	}
+
+  	if (rowData.imageurl) {
+  		var image = $("<img>");
+  		image.addClass("scrollBlockImage");
+  		image.attr("src", rowData.imageurl);
+  		block.append(image);
+  	} else if (rowData.videourl) {
+  		var video = $("<iframe frameborder='0' allow='accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture' allowfullscreen>");
+  		video.attr("src", rowData.videourl);
+  		block.append(video);
+  	}
+
+  	if (rowData.caption) {
+  		var caption = $("<div>");
+  		caption.addClass("scrollBlockCaption");
+  		caption.html(rowData.caption);
+  		block.append(caption);
+  	}
+
+  	$("#page").append(block);
+  }
+
+
+  renderSortedList(rowData) {
+    var sortedContainer;
+    if ($("#scroll"+rowData.heading).length) {
+      sortedContainer = $("#scroll"+rowData.heading);
+    } else {
+      sortedContainer = $("<div>");
+      sortedContainer.attr("id", "scroll"+rowData.heading);
+      var sortedContainerHeading = $("<div>");
+      sortedContainerHeading.html(rowData.heading);
+      sortedContainerHeading.addClass("sortedContainerHeading");
+      sortedContainer.append(sortedContainerHeading);
+      sortedContainer.addClass("sortedContainer");
+      $("#page").append(sortedContainer);
+    } 
+
+    var sortedLineText = makeDetailsLine([rowData.role, "<i>"+rowData.name+"</i>", rowData.site, rowData.place, rowData.text]);
+    if (sortedLineText !== "") {
+    	var sortedLine;
+    	if (rowData.texturl) {
+    		var sortedLine = $("<a>");
+    		sortedLine.attr("href", rowData.texturl);
+    		sortedLine.attr("target", "_blank");
+    		sortedLine.addClass("clickable line")
+	      sortedLine.html(sortedLineText);
+	      sortedContainer.append(sortedLine);
+    	} else {
+	      var sortedLine = $("<div>");
+	      sortedLine.html(sortedLineText);
+	      sortedContainer.append(sortedLine);
+	    }
+    }
+  }
+
   render() {
   	this.setupPage()
-  	$("#page").append("Coming Soon");
-    // console.log(this.data);
-    // for (var rowIndex in this.data.rows) {
-    // 	var row = this.data.rows[rowIndex];
 
-    // 	if (row["imageurl"] !== undefined) {
-	   //  	var image = $("<img>");
-		  //   image.attr("src", row["imageurl"]);
-		  //   image.addClass("scrollImage");
-		  //   $("#page").append(image);
-    // 	}
+  	var rows = this.subOptions[getCurrentHash()].rows;
+  	for (var row in rows) {
+  		var rowData = rows[row];
 
-	   //  if (row["title"] !== undefined) {
-	   //    var title = $("<div>");
-	   //    title.html(row["title"]);
-	   //    title.addClass("imageTitle");
-	   //    $("#page").append(title);
-	   //  }
+  		switch (rowData.template) {
+        case "intro":
+          this.renderIntro(rowData);
+          break;
+        case "block":
+          this.renderBlock(rowData);
+          break;
+        case "sortedList":
+          this.renderSortedList(rowData);
+          break;
+  		}
+  	}
 
-	   //  var detailsString = "";
-	   //  if (row["medium"] !== undefined) {
-	   //    detailsString+=row["medium"]+" &#8226; "
-	   //  }
-	   //  if (row["dimensions"] !== undefined) {
-	   //    detailsString+=row["dimensions"]+" &#8226; "
-	   //  }
-	   //  if (row["year"] !== undefined) {
-	   //    detailsString+=row["year"]+" &#8226; "
-	   //  }
-	   //  if (detailsString !== "") {
-	   //    var details = $("<div>");
-	   //    details.html(detailsString.substring(0, detailsString.length-9));
-	   //    details.addClass("scrollDetails");
-	   //    $("#page").append(details);
-	   //  }
-
-
-    // }
   }
 }
