ea99e24359100dcf5c8183047143ef10d589cfd8
Sam Ticknor <srticknor@gmail.com>
2023-03-01 23:12:11 -0500

    Updates

---
+++ precache-manifest.b19b3c5604018eef0c85b0ffdd08f710.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "4d2926bbd7ac3f25bf70",
+    "url": "/Thesis-Site/static/css/main.e8974543.chunk.css"
+  },
+  {
+    "revision": "4d2926bbd7ac3f25bf70",
+    "url": "/Thesis-Site/static/js/main.27eb27a9.chunk.js"
+  },
+  {
+    "revision": "fa7992f7da39dba450d9",
+    "url": "/Thesis-Site/static/js/runtime~main.cf51307c.js"
+  },
+  {
+    "revision": "062352c245951ccb39e9",
+    "url": "/Thesis-Site/static/js/2.ee27235a.chunk.js"
+  },
+  {
+    "revision": "a66966d18209964e0685",
+    "url": "/Thesis-Site/static/js/3.3b0f191d.chunk.js"
+  },
+  {
+    "revision": "1db1e42a842faf137a4e1556f3f13de7",
+    "url": "/Thesis-Site/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.dbad329523a76a0f70e7f485b01f9d76.js"
+  "/Thesis-Site/precache-manifest.b19b3c5604018eef0c85b0ffdd08f710.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/Thesis-Site/index.html", {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
