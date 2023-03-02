273c97c56a154cd7918d28213c683ee74431cc4a
Sam Ticknor <srticknor@gmail.com>
2023-03-01 23:04:18 -0500

    Updates

--- precache-manifest.dbad329523a76a0f70e7f485b01f9d76.js
+++ precache-manifest.dbad329523a76a0f70e7f485b01f9d76.js
@@ -1,26 +1,26 @@
 self.__precacheManifest = [
   {
-    "revision": "924c2f7020defcdd2045",
+    "revision": "4d2926bbd7ac3f25bf70",
     "url": "/static/css/main.e8974543.chunk.css"
   },
   {
-    "revision": "924c2f7020defcdd2045",
-    "url": "/static/js/main.3c005761.chunk.js"
+    "revision": "4d2926bbd7ac3f25bf70",
+    "url": "/static/js/main.27eb27a9.chunk.js"
   },
   {
     "revision": "b12b75d37321b97ebd1a",
     "url": "/static/js/runtime~main.afbdf9f3.js"
   },
   {
-    "revision": "8a68e22c2f5fb58b30ca",
-    "url": "/static/js/2.e1349962.chunk.js"
+    "revision": "21850052ba0c4dde9060",
+    "url": "/static/js/2.e7d5638d.chunk.js"
   },
   {
     "revision": "a66966d18209964e0685",
     "url": "/static/js/3.3b0f191d.chunk.js"
   },
   {
-    "revision": "5c79202a194798554a199ede8b7aacf3",
+    "revision": "4dacb8352c7c116e55149086839790cf",
     "url": "/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.182cf912a9a0e0f57c71aacd0ce13f40.js"
+  "/precache-manifest.dbad329523a76a0f70e7f485b01f9d76.js"
 );
 
 workbox.clientsClaim();
